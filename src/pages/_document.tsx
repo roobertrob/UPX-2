0916b200f0daf6f8c3559f726ad7b9e0e2acac76
roobertrob <robert.fonseca@stone.com.br>
2024-10-14 14:30:28 -0300

    feat: navbar finished

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,18 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
+
+import { PageHeader } from '@/components/PageHeader';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <body className="antialiased">
+        <PageHeader />
         <Main />
         <NextScript />
       </body>
