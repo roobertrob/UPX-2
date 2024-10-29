d51f51a4ade20e9ded4a0448de62e3d803eeaf9b
roobertrob <robert.fonseca@stone.com.br>
2024-10-28 22:09:03 -0300

    feat: layout adjustment

--- src/components/AnimalCard/index.tsx
+++ src/components/AnimalCard/index.tsx
@@ -2,11 +2,12 @@ import Image from 'next/image';
 
 import { Card } from '../Card';
 
-const AnimalCard = ({ imagePath, description }) => {
+const AnimalCard = ({ imagePath, description, title }) => {
   return (
     <Card withBorder>
       <Image src={imagePath} width={100} height={100} alt="" />
-      <p className='mt-4'>{description}</p>
+      <h3 className="text-3xl font-extrabold my-2">{title}</h3>
+      <p className="mt-4">{description}</p>
     </Card>
   );
 };
--- src/components/HomePage/AnimalsInExtinctionSection/index.tsx
+++ src/components/HomePage/AnimalsInExtinctionSection/index.tsx
@@ -5,21 +5,21 @@ import { AnimalCard } from '@/components/AnimalCard';
 const animalClasses = [
   {
     id: 'mammals',
-    title: 'Mamíferos',
+    title: 'MAMÍFEROS',
     description:
       'Mamíferos são vertebrados com glândulas mamárias e pelos. De sangue quente e cérebro desenvolvido, o que facilita a adaptação. Eles vivem em variados ambientes, desde oceanos até florestas.',
     imagePath: '/game-icons_deer.svg',
   },
   {
     id: 'reptiles',
-    title: 'Répteis',
+    title: 'RÉPTEIS',
     description:
       'Répteis são animais vertebrados de sangue frio, com pele geralmente coberta por escamas ou placas. Eles respiram por pulmões, a maioria se reproduz por ovos, e vivem em diversos ambientes.',
     imagePath: '/game-icons_sand-snake.svg',
   },
   {
     id: 'birds',
-    title: 'Aves',
+    title: 'AVES',
     description:
       'Aves são animais vertebrados de sangue quente, possuem penas, bico sem dentes e asas, que a maioria usa para voar. Elas se reproduzem por ovos, com cuidado parental após a eclosão.',
     imagePath: '/game-icons_egyptian-bird.svg',
@@ -36,6 +36,7 @@ const AnimalsInExtinctionSection = () => {
         {animalClasses.map((animalClass) => (
           <AnimalCard
             key={animalClass.id}
+            title={animalClass.title}
             imagePath={animalClass.imagePath}
             description={animalClass.description}
           />
