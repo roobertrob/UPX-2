72af13624e092741b31d096a13e753f2512e285c
Luana Melo <lunamelo.26@gmail.com>
2024-11-14 22:43:23 -0300

    Atualizando textos templates home

--- src/components/HomePage/IntroductionSection/index.tsx
+++ src/components/HomePage/IntroductionSection/index.tsx
@@ -3,14 +3,28 @@ const IntroductionSection = () => {
     <section className="max-w-6xl">
       <h1 className="text-2xl font-extrabold my-8">O QUE É EXTINÇÃO?</h1>
       <p className="text-xl">
-        Lorem ipsum dolor sit amet. Aut rerum porro sit dicta odio qui odit
-        molestias id dicta galisum et corrupti eius in culpa natus At inventore
-        esse? Ut voluptatem veritatis sed nisi suscipit ut quis odit et Quis
-        rerum ut enim autem sed eligendi repellat ea cupiditate veniam? Qui
-        distinctio deleniti a inventore eveniet ut consequatur quisquam ab eius
-        unde cum assumenda culpa! Est nesciunt officia est vitae temporibus eos
-        autem perspiciatis et omnis aliquam et optio corporis et voluptates
-        odit. Ut adipisci odit ut molestiae eius sit quia commodi.
+        A extinção é um fenômeno natural que marca o desaparecimento completo de
+        uma espécie. Esse processo pode ocorrer de forma gradual, ao longo de
+        milhares de anos, ou de maneira abrupta, como resultado de eventos
+        catastróficos.
+        <br />
+        <br />
+        As causas da extinção são diversas e podem incluir fatores naturais,
+        como mudanças climáticas, erupções vulcânicas e terremotos, ou fatores
+        causados pela ação humana, como a destruição de habitats, a caça e a
+        pesca excessivas, a poluição e a introdução de espécies invasoras. Ela é
+        um processo irreversível e tem consequências significativas para os
+        ecossistemas, pois cada espécie desempenha um papel importante na
+        manutenção do equilíbrio ambiental.
+        <br />
+        <br />
+        A extinção de espécies representa uma perda irreparável para a
+        biodiversidade do planeta. Cada espécie extinta é um elo perdido na
+        cadeia da vida, e sua ausência pode desencadear uma série de efeitos em
+        cascata, afetando outras espécies e os ecossistemas como um todo. A
+        perda da biodiversidade pode levar à diminuição da produtividade dos
+        ecossistemas, à fragilização das redes tróficas e à redução da
+        capacidade dos ecossistemas de se recuperarem de distúrbios.
       </p>
     </section>
   );
--- src/components/HomePage/WhatIsEcoWarriors/index.tsx
+++ src/components/HomePage/WhatIsEcoWarriors/index.tsx
@@ -14,18 +14,21 @@ const WhatIsEcoWarriors = () => {
         <h3 className="text-3xl">O QUE É O ECOWARRIORS?</h3>
         <div className="text-start space-y-2 my-8">
           <p>
-            Lorem ipsum dolor sit amet. Aut rerum porro sit dicta odio qui odit
-            molestias id dicta galisum et corrupti eius in culpa natus At
-            inventore esse? Ut voluptatem veritatis sed nisi suscipit ut quis
-            odit et Quis rerum ut enim autem sed eligendi repellat ea cupiditate
-            veniam?
+            EcoWarriors é mais do que apenas um site, é um convite para você se
+            tornar um defensor da vida selvagem! Nossa missão é conectar pessoas
+            de todas as idades com a incrível biodiversidade das Américas,
+            especialmente do Brasil. Através de conteúdos divertidos e
+            informativos, você vai descobrir um mundo fascinante de animais em
+            risco de extinção e entender a importância de preservá-los.
           </p>
           <p>
-            Qui distinctio deleniti a inventore eveniet ut consequatur quisquam
-            ab eius unde cum assumenda culpa! Est nesciunt officia est vitae
-            temporibus eos autem perspiciatis et omnis aliquam et optio corporis
-            et voluptates odit. Ut adipisci odit ut molestiae eius sit quia
-            commodi.
+            No EcoWarriors, você encontrará jogos divertidos que te farão
+            aprender sobre as características, habitats e ameaças que essas
+            espécies enfrentam. Além disso, você terá a oportunidade de conhecer
+            ONGs inspiradoras de pessoas que estão trabalhando para proteger a
+            natureza. Junte-se a nós nessa jornada e faça a diferença! Afinal,
+            cada um de nós pode contribuir para um futuro mais verde e
+            sustentável.
           </p>
         </div>
         <Link
